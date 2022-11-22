5efc60b68e763b5c53be937d5cefe3442afa06ed
huamingduo <song.boyang5591@outlook.com>
2022-11-22 11:59:07 +0800

    format vins_estimator visualization code
    Signed-off-by: huamingduo <song.boyang5591@outlook.com>

--- vins_estimator/src/utility/visualization.h
+++ vins_estimator/src/utility/visualization.h
@@ -1,23 +1,27 @@
 #pragma once
 
+#include <geometry_msgs/PointStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
 #include <ros/ros.h>
-#include <std_msgs/Header.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Bool.h>
+#include <sensor_msgs/Image.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
-#include <nav_msgs/Path.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/PointStamped.h>
-#include <visualization_msgs/Marker.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Header.h>
 #include <tf/transform_broadcaster.h>
-#include "CameraPoseVisualization.h"
+#include <visualization_msgs/Marker.h>
+
 #include <eigen3/Eigen/Dense>
+#include <fstream>
+
 #include "../estimator.h"
 #include "../parameters.h"
-#include <fstream>
+#include "CameraPoseVisualization.h"
+
+using namespace vins;
 
 extern ros::Publisher pub_odometry;
 extern ros::Publisher pub_path, pub_pose;
